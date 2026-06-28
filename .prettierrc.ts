f92efdb35a1448b335c1c3620436ce9646b6ebb8
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2026-06-28 17:23:55 +0500

    update packages

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -11,7 +11,7 @@ const config: Config = {
 	useTabs: true,
 	objectWrap: 'collapse',
 	tabWidth: 2,
-	plugins: ['prettier-plugin-tailwindcss', "prettier-plugin-organize-imports"],
+	plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-organize-imports'],
 	tailwindFunctions: ['cn'],
 }
 
