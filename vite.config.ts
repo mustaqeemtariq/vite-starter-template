37298fed299f50c38cadd8270ce83c348938b5b8
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2026-06-28 17:35:11 +0500

    fix eslint error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+// eslint-disable-next-line import/default
 import svgrPlugin from 'vite-plugin-svgr'
 
 export default defineConfig({
